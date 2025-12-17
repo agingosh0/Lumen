538f47df34276645343fe2c2f49b9a03579feb14
Agin Gosh <agin@zfloc.com>
2025-12-18 04:48:41 +0530

    Update CORS configuration and add environment key generator

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,42 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'market-assets.strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'market-assets.strapi.io',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: process.env.CORS_ORIGIN 
+        ? process.env.CORS_ORIGIN.split(',') 
+        : ['*'], // Allow all in development, set specific domains in production
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
---
+++ generate-env-keys.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/**
+ * Script to generate secure environment variables for Strapi deployment
+ * Run: node generate-env-keys.js
+ */
+
+const crypto = require('crypto');
+
+function generateKey() {
+  return crypto.randomBytes(32).toString('base64');
+}
+
+function generateKeys() {
+  const keys = [];
+  for (let i = 0; i < 4; i++) {
+    keys.push(generateKey());
+  }
+  return keys.join(',');
+}
+
+console.log('\n🔐 Generate Strapi Environment Variables\n');
+console.log('Copy these values to your .env.production file or hosting platform:\n');
+console.log('='.repeat(60));
+console.log('\nAPP_KEYS=' + generateKeys());
+console.log('API_TOKEN_SALT=' + generateKey());
+console.log('ADMIN_JWT_SECRET=' + generateKey());
+console.log('TRANSFER_TOKEN_SALT=' + generateKey());
+console.log('JWT_SECRET=' + generateKey());
+console.log('\n' + '='.repeat(60));
+console.log('\n✅ Generated! Copy these to your environment variables.\n');
+
